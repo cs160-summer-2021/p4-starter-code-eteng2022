b95c80ab8da43561e6a934dc24a01a85278beed8
Emily Teng <eteng2022@berkeley.edu>
2021-07-26 01:00:52 -0700

    completed taskflux taskboard

--- draw/urls.py
+++ draw/urls.py
@@ -4,7 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('test/', views.test, name='test'),
+    path('taskboard/', views.taskboard, name='taskboard'),
+    path('phone/', views.phone, name='phone'),
+    path('add/', views.add, name='add'),
+    path('login/', views.login, name='login'),
     path('<str:room_name>/', views.room, name='room'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,21 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def test(request):
+    return render(request, 'draw/test.html')
+
+def taskboard(request):
+    return render(request, 'draw/taskboard.html')
+
+def phone(request):
+    return render(request, 'draw/phone.html')
+
+def add(request):
+    return render(request, 'draw/add.html')
+
+def login(request):
+    return render(request, 'draw/login.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
